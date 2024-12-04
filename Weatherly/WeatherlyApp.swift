010127edef7b89ed312c6f4dd55aee021ac02f02
Adil Rahmani <didilusse@gmail.com>
2024-12-04 18:29:25 -0500

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Adil Rahmani on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

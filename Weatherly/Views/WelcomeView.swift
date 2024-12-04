790bc4f4cb881cb53d1fe2120a87614cf333c348
Adil Rahmani <didilusse@gmail.com>
2024-12-04 18:37:26 -0500

    created location manager

---
+++ Weatherly/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Weatherly
+//
+//  Created by Adil Rahmani on 12/4/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
